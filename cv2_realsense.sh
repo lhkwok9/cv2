3d9d2a3444bf3bffb6bbd15e063ca31f6f8045ad
JK <s2007klhwaiommk@gmail.com>
2022-01-07 21:56:30 +0800

    Create cv2_realsense.sh

---
+++ cv2_realsense.sh
@@ -0,0 +1,136 @@
+#!/bin/bash
+set -e
+echo "It will take minimal 5.5 hour !"
+cd ~
+
+sudo apt-get update && sudo apt-get upgrade
+
+# install the opencv dependencies
+sudo apt-get install -y build-essential cmake git pkg-config
+sudo apt-get install -y libjpeg-dev libtiff-dev libpng-dev
+sudo apt-get install -y libavcodec-dev libavformat-dev libswscale-dev
+sudo apt-get install -y libgtk2.0-dev libcanberra-gtk* libgtk-3-dev
+sudo apt-get install -y libgstreamer1.0-dev gstreamer1.0-gtk3
+sudo apt-get install -y libgstreamer-plugins-base1.0-dev gstreamer1.0-gl
+sudo apt-get install -y libxvidcore-dev libx264-dev
+
+sudo apt-get install -y python3-dev python3-numpy python3-pip
+sudo apt-get install -y libtbb2 libtbb-dev libdc1394-22-dev
+sudo apt-get install -y libv4l-dev v4l-utils
+sudo apt-get install -y libopenblas-dev libatlas-base-dev libblas-dev
+sudo apt-get install -y liblapack-dev gfortran libhdf5-dev
+sudo apt-get install -y libprotobuf-dev libgoogle-glog-dev libgflags-dev
+sudo apt-get install -y protobuf-compiler
+
+# install the realsense dependencies
+sudo apt-get install libssl-dev
+sudo apt-get install automake libtool vim cmake libusb-1.0-0-dev libx11-dev xorg-dev libglu1-mesa-dev
+
+# remove old opencv versions
+cd ~ 
+sudo rm -rf opencv*
+
+# download OpenCV
+git clone https://github.com/opencv/opencv.git
+cd ~/opencv
+git checkout 3.4
+cd ~
+git clone https://github.com/opencv/opencv_contrib.git
+cd ~/opencv_contrib
+git checkout 3.4
+cd ~
+
+# set install dir
+cd ~/opencv
+mkdir build
+cd build
+
+# run cmake
+cmake -D CMAKE_BUILD_TYPE=RELEASE \
+-D CMAKE_INSTALL_PREFIX=/usr/local \
+-D OPENCV_EXTRA_MODULES_PATH=~/opencv_contrib/modules \
+-D ENABLE_NEON=ON \
+-D ENABLE_VFPV3=ON \
+-D WITH_OPENMP=ON \
+-D BUILD_ZLIB=ON \
+-D BUILD_TIFF=ON \
+-D WITH_FFMPEG=ON \
+-D WITH_TBB=ON \
+-D BUILD_TBB=ON \
+-D BUILD_TESTS=OFF \
+-D WITH_EIGEN=OFF \
+-D WITH_GSTREAMER=ON \
+-D WITH_V4L=ON \
+-D WITH_LIBV4L=ON \
+-D WITH_VTK=OFF \
+-D WITH_QT=OFF \
+-D OPENCV_ENABLE_NONFREE=ON \
+-D INSTALL_C_EXAMPLES=OFF \
+-D INSTALL_PYTHON_EXAMPLES=OFF \
+-D BUILD_NEW_PYTHON_SUPPORT=ON \
+-D BUILD_opencv_python3=TRUE \
+-D OPENCV_GENERATE_PKGCONFIG=ON \
+-D BUILD_EXAMPLES=OFF ..
+
+# run make
+make -j4
+sudo make install
+sudo ldconfig
+
+# cleaning (frees 300 MB)
+make clean
+sudo apt-get update
+
+#finish building opencv
+export OpenCV_DIR=~/opencv/build
+
+# remove old realsense versions
+cd ~ 
+sudo rm -rf librealsense*
+
+# download Realsense
+cd ~
+git clone https://github.com/IntelRealSense/librealsense.git
+
+# create and apply a new udev rule
+cd librealsense
+sudo cp config/99-realsense-libusb.rules /etc/udev/rules.d/
+sudo udevadm control --reload-rules && sudo udevadm trigger
+
+# modify the path
+sudo echo 'export LD_LIBRARY_PATH=/usr/local/lib:$LD_LIBRARY_PATH' >> ~/.bashrc
+source ~/.bashrc
+
+# install protobuf — Google's language-neutral, platform-neutral, extensible mechanism for serializing structured data
+cd ~
+git clone --depth=1 -b v3.10.0 https://github.com/google/protobuf.git
+cd protobuf
+./autogen.sh
+./configure
+make -j1
+sudo make install
+cd python
+export LD_LIBRARY_PATH=../src/.libs
+python3 setup.py build --cpp_implementation 
+python3 setup.py test --cpp_implementation
+sudo python3 setup.py install --cpp_implementation
+export PROTOCOL_BUFFERS_PYTHON_IMPLEMENTATION=cpp
+export PROTOCOL_BUFFERS_PYTHON_IMPLEMENTATION_VERSION=3
+sudo ldconfig
+protoc --version
+
+# install libtbb-dev parallelism library for C++
+cd ~
+wget https://github.com/PINTO0309/TBBonARMv7/raw/master/libtbb-dev_2018U2_armhf.deb
+sudo dpkg -i ~/libtbb-dev_2018U2_armhf.deb
+sudo ldconfig
+rm libtbb-dev_2018U2_armhf.deb
+
+# install RealSense SDK librealsense
+cd ~/librealsense
+mkdir  build  && cd build
+cmake .. -DBUILD_EXAMPLES=true -DCMAKE_BUILD_TYPE=Release -DFORCE_LIBUVC=true
+make -j1
+sudo make install
+
+echo "Done !"
